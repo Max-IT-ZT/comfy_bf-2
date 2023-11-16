4ab3d34c84737d4967f8095861f6a72ca9faa4c2
Max-IT-ZT <babuskomax@gmail.com>
2023-11-16 23:53:28 +0200

    fix

--- js/main.js
+++ js/main.js
@@ -1,31 +1,31 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const openBtn = document.querySelector(".js-open-modal");
-  const modal = document.getElementById("modal");
-  const overlay = document.querySelector(".overlay");
-  const closeBtns = document.querySelectorAll(".js-modal-close");
+// document.addEventListener("DOMContentLoaded", function () {
+//   const openBtn = document.querySelector(".js-open-modal");
+//   const modal = document.getElementById("modal");
+//   const overlay = document.querySelector(".overlay");
+//   const closeBtns = document.querySelectorAll(".js-modal-close");
 
-  openBtn.addEventListener("click", function () {
-    modal.style.display = "block";
-    overlay.style.display = "block";
-  });
+//   openBtn.addEventListener("click", function () {
+//     modal.style.display = "block";
+//     overlay.style.display = "block";
+//   });
 
-  closeBtns.forEach(function (closeBtn) {
-    closeBtn.addEventListener("click", function () {
-      modal.style.display = "none";
-      overlay.style.display = "none";
-    });
-  });
+//   closeBtns.forEach(function (closeBtn) {
+//     closeBtn.addEventListener("click", function () {
+//       modal.style.display = "none";
+//       overlay.style.display = "none";
+//     });
+//   });
 
-  overlay.addEventListener("click", function () {
-    modal.style.display = "none";
-    overlay.style.display = "none";
-  });
+//   overlay.addEventListener("click", function () {
+//     modal.style.display = "none";
+//     overlay.style.display = "none";
+//   });
 
-  document.body.addEventListener("keyup", function (e) {
-    const key = e.keyCode;
-    if (key === 27) {
-      modal.style.display = "none";
-      overlay.style.display = "none";
-    }
-  });
-});
+//   document.body.addEventListener("keyup", function (e) {
+//     const key = e.keyCode;
+//     if (key === 27) {
+//       modal.style.display = "none";
+//       overlay.style.display = "none";
+//     }
+//   });
+// });
